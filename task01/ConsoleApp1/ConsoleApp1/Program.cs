b9e1fb3f065a8769be12dc46d9ab2c047a8ec1cb
s27247 <s27247@pjwstk.edu.pl>
2024-03-16 21:05:53 +0100

    Implemented a feature for calculating average

--- task01/ConsoleApp1/ConsoleApp1/Program.cs
+++ task01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,16 @@ Console.WriteLine("This is modification 1");
 Console.WriteLine("This is modification 2");
 
 Console.WriteLine("This is modification 3:))");
+
+ double CalculateAverage(int[] a)
+ {
+     int s = 0;
+     foreach(int n in a)
+     {
+         s += n;
+     }
+     return (double)s/a.Length;
+ }
+ 
+ int[] arr = {1,2,3,4,5,6};
+ Console.WriteLine("average is: " + CalculateAverage(arr));
