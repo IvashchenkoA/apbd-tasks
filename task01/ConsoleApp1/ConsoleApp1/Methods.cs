4d40c3de215b17073d1f81c56de95bbb558cbe02
s27247 <s27247@pjwstk.edu.pl>
2024-03-16 21:25:25 +0100

    Implemented a feature to find maximum

---
+++ task01/ConsoleApp1/ConsoleApp1/Methods.cs
@@ -0,0 +1,25 @@
+namespace ConsoleApp1;
+
+public class Methods
+{
+    public static double CalculateAverage(int[] a)
+    {
+        int s = 0;
+        foreach(int n in a)
+        {
+            s += n;
+        }
+        return (double)s/a.Length;
+    }
+
+    public static int FindMax(int[] a)
+    {
+        int mx = a[0];
+        for (int i = 1; i < a.Length; i++)
+        {
+            mx = Math.Max(a[i],mx);
+        }
+
+        return mx;
+    }
+}
--- task01/ConsoleApp1/ConsoleApp1/Program.cs
+++ task01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This is modification 1");
 
@@ -6,15 +8,9 @@ Console.WriteLine("This is modification 2");
 
 Console.WriteLine("This is modification 3:))");
 
- double CalculateAverage(int[] a)
- {
-     int s = 0;
-     foreach(int n in a)
-     {
-         s += n;
-     }
-     return (double)s/a.Length;
- }
+ 
  
  int[] arr = {1,2,3,4,5,6};
- Console.WriteLine("average is: " + CalculateAverage(arr));
+ Console.WriteLine("average is: " + Methods.CalculateAverage(arr));
+ 
+ Console.WriteLine("maximum in an array is: " + Methods.FindMax(arr));
